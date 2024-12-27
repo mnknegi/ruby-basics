993001251595e65e7e4a85ced8ca59ca87afa781
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-27 19:30:29 +0530

    Introduction to escape character.

---
+++ learn-to-code-with-ruby/02 - Getting Started/escape_characters.rb
@@ -0,0 +1,12 @@
+puts "Some text\nNew line text."
+puts "\tOnce upon a time"
+
+puts
+
+puts "Juliet said \"Goodbye\" to Romeo."
+puts 'Juliet said \'Goodbye\' to Romeo.'
+
+puts
+
+puts 'Juliet said "Goodbye" to Romeo.'
+puts "Juliet said 'Goodbye' to Romeo."
