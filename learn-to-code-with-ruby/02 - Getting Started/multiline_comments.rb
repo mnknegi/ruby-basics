7761d0b96f2fef7b152034b10c350851cb46bc55
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-27 20:14:23 +0530

    Multiline comment.

---
+++ learn-to-code-with-ruby/02 - Getting Started/multiline_comments.rb
@@ -0,0 +1,8 @@
+=begin
+This is a multiline comment.
+All the lines between begin and end will be grayed out.
+This means these lines of code will not be executed by ruby interpreter.
+We can also select multiple lines and press command + / to comment more than one line.
+=end
+
+puts 1 + 2 # This will be executed.
