10c97a4852f4148658279a728c31e1d01ebd6b08
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-27 20:10:28 +0530

    Single line comment.

---
+++ learn-to-code-with-ruby/02 - Getting Started/comments.rb
@@ -0,0 +1,3 @@
+
+# Addition of two numbers. command + /
+puts 1 + 1
---
+++ learn-to-code-with-ruby/02 - Getting Started/the_p_method.rb
@@ -0,0 +1,15 @@
+puts "Hello, World"
+p "Hello, World"
+
+puts "5"
+puts 5
+
+puts
+
+p "5"
+p 5
+
+p 3.14159
+
+puts "Hi there \nLine break."
+p "Hi there \nLine break."
