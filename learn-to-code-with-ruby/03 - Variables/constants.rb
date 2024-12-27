fc749d4e2472e2e612119c34f4bf0c15c121f639
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-27 22:36:47 +0530

    constants

---
+++ learn-to-code-with-ruby/03 - Variables/constants.rb
@@ -0,0 +1,9 @@
+pi = 3.14159 # pi is variable
+PI = 3.14159 # pi is constant by using first uppercase letter.
+
+TAX_RATE = 18
+puts PI 
+puts TAX_RATE
+
+TAX_RATE = 30 # will produce a warning.
+puts TAX_RATE
