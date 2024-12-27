597cd4da18902c075c45592bd456edd239d1f498
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-27 19:20:27 +0530

    Puts command

---
+++ learn-to-code-with-ruby/02 - Getting Started/the_puts_method.rb
@@ -0,0 +1,18 @@
+3 + 5 # will evaluate but do not produce a result
+
+puts 1
+puts 1 + 1
+
+puts "Hello, World!"
+
+puts 3.14159
+
+puts
+
+puts "Afterwords"
+
+puts 1
+puts 2
+puts 3
+
+puts 1, 2, 3
