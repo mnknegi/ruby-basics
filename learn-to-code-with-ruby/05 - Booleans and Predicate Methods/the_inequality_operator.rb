1460898337420a0bd929d39c97e643421f22b684
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 18:21:30 +0530

    inequality operator.

---
+++ learn-to-code-with-ruby/05 - Booleans and Predicate Methods/the_inequality_operator.rb
@@ -0,0 +1,17 @@
+puts "hello" != "hello"
+puts "hello" != "hello "
+puts "hello" != "Hello"
+
+puts
+
+puts "5".to_i != 5
+puts 5.to_s != "5"
+
+puts
+
+puts "5" != 5
+ 
+puts
+
+puts 5 != 5.0 # logically equal in Maths so is in Ruby.
+puts 5 != 5.1
