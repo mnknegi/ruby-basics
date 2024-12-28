006a29a5cedd81bc9f9faa3fc44db76bef23b25e
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 20:38:30 +0530

    float methods.

---
+++ learn-to-code-with-ruby/05 - Booleans and Predicate Methods/float_methods.rb
@@ -0,0 +1,25 @@
+puts 3.14159.floor
+puts 3.8.floor
+puts 3.8.floor.class
+
+puts
+
+puts 3.14159.ceil
+puts 3.8.ceil
+puts 3.8.ceil.class 
+
+puts
+
+puts 3.14159.round
+puts 3.8.round
+puts 3.5.round
+puts 3.8.round.class
+puts 3.14859.round
+puts 3.14859.round(2)
+
+puts
+
+puts 3.14159.abs
+puts -3.8.abs
+puts 3.5.abs
+puts -3.8.abs.class
