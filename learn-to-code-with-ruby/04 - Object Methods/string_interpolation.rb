195b4c6fca8bd2a6686676c4a8d33dd51d42cd0e
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 15:18:33 +0530

    String Interpolation.

---
+++ learn-to-code-with-ruby/04 - Object Methods/string_interpolation.rb
@@ -0,0 +1,12 @@
+name = "Mayank"
+
+puts "My name is #{name}"
+
+age = 31
+# puts "My age is " + age + "." # This will produce an error.
+puts "My age is #{age}."
+
+puts "After 10 years I will be #{age + 10} years old."
+
+x, y = 2, 4
+puts "The sum of #{x} and #{y} is #{x + y}."
