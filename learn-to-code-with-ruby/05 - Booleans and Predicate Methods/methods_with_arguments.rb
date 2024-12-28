c36cba3998964a6a3ae57bc2c106b52ea103be9c
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 18:33:22 +0530

    methods with arguments.

---
+++ learn-to-code-with-ruby/05 - Booleans and Predicate Methods/methods_with_arguments.rb
@@ -0,0 +1,9 @@
+puts "Mayank Negi".include?("M")
+puts "Mayank Negi".include?("N")
+puts "Mayank Negi".include?("z")
+puts "Mayank Negi".include?("m")
+puts "Mayank Negi".include? "y"
+
+# puts "Mayank Negi".include? # ArgumentError.
+
+puts ("Hello, World!")
