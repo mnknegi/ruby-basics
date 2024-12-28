064398b2067d4be8a5fcfa86ce979119f31cf617
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 18:26:40 +0530

    predicate method.

---
+++ learn-to-code-with-ruby/05 - Booleans and Predicate Methods/predicate_methods.rb
@@ -0,0 +1,19 @@
+# object.method?
+
+puts 10.even?
+puts 10.odd?
+
+puts
+
+puts 11.even?
+puts 11.odd?
+
+puts
+
+puts 10.positive?
+puts 10.negative?
+
+puts
+
+puts -10.positive?
+puts -10.negative?
