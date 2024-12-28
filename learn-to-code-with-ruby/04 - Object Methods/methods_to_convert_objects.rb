3364a83079440bd745c1fa0e0740b25d764ea107
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 15:50:59 +0530

    conversions

---
+++ learn-to-code-with-ruby/04 - Object Methods/methods_to_convert_objects.rb
@@ -0,0 +1,28 @@
+text = 5
+puts text.class
+puts text.to_i
+puts text.to_i.class
+
+puts
+
+puts text.class
+puts text.to_f
+puts text.to_f.class
+puts text.to_f.to_i
+
+puts
+
+puts text.class
+puts text.to_f
+puts text.to_f.class
+
+puts 
+
+puts 5.to_s
+puts 5.to_s.class
+puts 10.2.to_s
+puts 10.2.to_s.class
+
+puts "15 apples.".to_i
+puts "apples 15.".to_i
+puts "apples".to_i
