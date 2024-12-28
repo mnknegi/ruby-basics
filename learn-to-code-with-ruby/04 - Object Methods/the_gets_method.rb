15c90f3dbcc55302ccc6741af9dc31b00e618295
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 15:28:33 +0530

    Introduction of gets method.

---
+++ learn-to-code-with-ruby/04 - Object Methods/the_gets_method.rb
@@ -0,0 +1,9 @@
+puts "Hi, Please enter your name:"
+
+name = gets.chomp # chomp is used to remove extra linebreak.
+
+puts "Thanks you, now please enter your age:"
+
+age = gets.chomp
+
+puts "Welcome #{name}, your age is #{age}."
