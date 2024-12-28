f52b32692d79271ad524c9684b6806d505d619eb
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-28 18:38:07 +0530

    methods with multiple arguments.

---
+++ learn-to-code-with-ruby/05 - Booleans and Predicate Methods/methods_with_multiple_arguments.rb
@@ -0,0 +1,10 @@
+puts 20.between? 10, 30
+puts 20.between? 10, 15
+puts 20.between?(10, 30)
+puts 20.between?(20, 30)
+puts 20.between?(10, 20)
+puts 20.between?(20, 20)
+
+puts 1.2.between?(1.0, 1.5)
+puts -1.2.between?(-1.0, -1.5)
+puts 1.2.between?(1, 2)
